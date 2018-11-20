cf88e6c019465fd7bae0b2ffd27b69763fa925fa
aadrian <a.adrian.tech@googlemail.com>
2018-11-20 12:36:30 +0100

    refactor selenium code to extra package.

---
+++ src/main/java/com/mycorp/Selenium.java
@@ -0,0 +1,73 @@
+package com.mycorp;
+
+import org.apache.commons.lang3.SystemUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Selenium {
+    public WebDriver initFirefox() {
+        System.out.println("Selenium.initFirefox");
+        // OS independent way of building the WebDriver path
+        String driverName = "geckodriver";
+        String os = null;
+
+        // Gekodriver contains the version in the directory name too (unlike Chrome)
+        // Todo: change it here too, when changing in gradle.properties
+        String gekodriverVersion = "v0.23.0";
+
+        if(SystemUtils.IS_OS_WINDOWS) {
+            driverName+=".exe"; // windows files contain the .exe
+            os = "win64";
+        } else if(SystemUtils.IS_OS_MAC_OSX) {
+            os = "macos";
+        } else if(SystemUtils.IS_OS_LINUX) {
+            os = "linux64";
+        } else {
+            throw new RuntimeException("Unsupported OS");
+        }
+
+        String driverFileName = "drivers/geckodriver-"+gekodriverVersion+"-"+os+"/"+driverName;
+        System.out.println("using driverFileName = " + driverFileName);
+
+        // set the driver location
+        System.setProperty("webdriver.gecko.driver", driverFileName);
+        return new FirefoxDriver();
+    }
+
+    public WebDriver initChrome() {
+        System.out.println("Selenium.initChrome");
+        // OS independent way of building the WebDriver path
+        String driverName = "chromedriver";
+        String os = null;
+        if(SystemUtils.IS_OS_WINDOWS) {
+            driverName+=".exe"; // windows files contain the .exe
+            os = "win32";
+        } else if(SystemUtils.IS_OS_MAC_OSX) {
+            os = "mac64";
+        } else if(SystemUtils.IS_OS_LINUX) {
+            os = "linux64";
+        } else {
+            throw new RuntimeException("Unsupported OS");
+        }
+
+        String driverFileName = "drivers/chromedriver_"+os+"/"+driverName;
+        System.out.println("using driverFileName = " + driverFileName);
+
+        // set the driver location
+        System.setProperty("webdriver.chrome.driver", driverFileName);
+        return new ChromeDriver();
+    }
+
+    public boolean step_navigate(WebDriver driver) {
+        System.out.println("Selenium.step_navigate");
+        // navigate to Google to test if it works ...
+        driver.get("https://google.com");
+        return true;
+    }
+
+    public boolean step_check_title(WebDriver driver) {
+        System.out.println("Selenium.step_check_title");
+        return driver.getTitle().toLowerCase().contains("Google");
+    }
+}
